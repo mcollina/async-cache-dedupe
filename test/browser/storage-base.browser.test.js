7abadadc64971798984f5ccd73f74a2c34d09dd8
mateonunez <mateonunez95@gmail.com>
2023-02-06 19:55:42 +0100

    test(storage): add more specific tests

--- test/browser/storage-base.browser.test.js
+++ test/browser/storage-base.browser.test.js
@@ -1,33 +1,15 @@
 'use strict'
 
 const createStorage = require('../../src/storage')
-const StorageInterface = require('../../src/storage/interface')
 const { kAsyncCacheDedupeSuiteName, kAsyncCacheDedupeSuiteHasMultipleTests } = require('./helpers/symbols.js')
 
 module.exports = async function (test) {
-  test('should get an instance with default options', async (t) => {
-    const storage = createStorage()
+  test('should fail when using redis storage in the browser', async (t) => {
+    t.plan(1)
 
-    t.ok(typeof storage.get === 'function')
-    t.ok(typeof storage.set === 'function')
-    t.ok(typeof storage.remove === 'function')
-    t.ok(typeof storage.invalidate === 'function')
-    t.ok(typeof storage.refresh === 'function')
-  })
-
-  test('should get an error implementing storage interfaces without get method', async (t) => {
-    class BadStorage extends StorageInterface { }
-
-    const badStorage = new BadStorage()
-
-    for (const method of ['get', 'set', 'remove', 'invalidate', 'clear', 'refresh']) {
-      try {
-        await badStorage[method]()
-        t.fail(`should throw an error on method ${method}`)
-      } catch (err) {
-        t.equal(err.message, `storage ${method} method not implemented`)
-      }
-    }
+    t.throws(() => {
+      createStorage('redis', { client: {} })
+    }, { message: 'Redis storage is not supported in the browser' })
   })
 }
 
