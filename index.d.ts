a2d22361a096201b1639895bb3f8e9823d3053d3
Matthias <7304269+hmbrg@users.noreply.github.com>
2023-02-14 19:00:59 +0100

    Add transformer option (#50)
    * add transformer
    
    * add await to transformer deserialize
    
    * add transformer documentation
    
    * use correct get method in wrapper & fix transformer tests
    
    * fix coverage

--- index.d.ts
+++ index.d.ts
@@ -26,6 +26,11 @@ interface StorageMemoryOptions {
   invalidation?: boolean;
 }
 
+interface DataTransformer {
+  serialize: (data: any) => any;
+  deserialize: (data: any) => any;
+}
+
 type Events = {
   onDedupe?: (key: string) => void;
   onError?: (err: any) => void;
@@ -63,6 +68,7 @@ declare function createCache(
   options?: {
     storage?: StorageInputRedis | StorageInputMemory;
     ttl?: number;
+    transformer?: DataTransformer;
   } & Events
 ): Cache;
 
@@ -79,6 +85,7 @@ declare class Cache {
     name: string,
     opts: {
       storage?: StorageOptionsType;
+      transformer?: DataTransformer;
       ttl?: number;
       serialize?: (...args: any[]) => any;
       references?: (...args: any[]) => References | Promise<References>;
