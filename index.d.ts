54e43ed370109eb678dbe6c516082b3dbe6ab2c6
Alisson <thelinuxlich@gmail.com>
2023-04-06 18:46:35 -0300

    Add smarter types to references parameter in cache.define

--- index.d.ts
+++ index.d.ts
@@ -5,123 +5,112 @@ type StorageOptionsType = "redis" | "memory";
 type References = string | string[];
 
 interface LoggerInput {
-  msg: string;
-  [key: string]: any;
+	msg: string;
+	[key: string]: any;
 }
 
 interface Logger {
-  debug: (input: LoggerInput) => void;
-  warn: (input: LoggerInput) => void;
-  error: (input: LoggerInput) => void;
+	debug: (input: LoggerInput) => void;
+	warn: (input: LoggerInput) => void;
+	error: (input: LoggerInput) => void;
 }
 interface StorageRedisOptions {
-  client: Redis;
-  log?: Logger;
-  invalidation?: { referencesTTL: number } | boolean;
+	client: Redis;
+	log?: Logger;
+	invalidation?: { referencesTTL: number } | boolean;
 }
 
 interface StorageMemoryOptions {
-  size?: number;
-  log?: Logger;
-  invalidation?: boolean;
+	size?: number;
+	log?: Logger;
+	invalidation?: boolean;
 }
 
 interface DataTransformer {
-  serialize: (data: any) => any;
-  deserialize: (data: any) => any;
+	serialize: (data: any) => any;
+	deserialize: (data: any) => any;
 }
 
 type Events = {
-  onDedupe?: (key: string) => void;
-  onError?: (err: any) => void;
-  onHit?: (key: string) => void;
-  onMiss?: (key: string) => void;
+	onDedupe?: (key: string) => void;
+	onError?: (err: any) => void;
+	onHit?: (key: string) => void;
+	onMiss?: (key: string) => void;
 };
 
 type StorageInputRedis = {
-  type: "redis";
-  options?: StorageRedisOptions;
+	type: "redis";
+	options?: StorageRedisOptions;
 };
 
 type StorageInputMemory = {
-  type: "memory";
-  options?: StorageMemoryOptions;
+	type: "memory";
+	options?: StorageMemoryOptions;
 };
 
 declare class StorageInterface {
-  constructor(options: any);
+	constructor(options: any);
 
-  get(key: string): Promise<undefined | any>;
-  set(
-    key: string,
-    value: any,
-    ttl: number,
-    references?: References
-  ): Promise<void>;
-  remove(key: string): Promise<void>;
-  invalidate(references: References): Promise<void>;
-  clear(name: string): Promise<void>;
-  refresh(): Promise<void>;
+	get(key: string): Promise<undefined | any>;
+	set(key: string, value: any, ttl: number, references?: References): Promise<void>;
+	remove(key: string): Promise<void>;
+	invalidate(references: References): Promise<void>;
+	clear(name: string): Promise<void>;
+	refresh(): Promise<void>;
 }
 
 declare function createCache(
-  options?: {
-    storage?: StorageInputRedis | StorageInputMemory;
-    ttl?: number;
-    transformer?: DataTransformer;
-    stale?: number;
-  } & Events
+	options?: {
+		storage?: StorageInputRedis | StorageInputMemory;
+		ttl?: number;
+		transformer?: DataTransformer;
+		stale?: number;
+	} & Events,
 ): Cache;
 
 declare class Cache {
-  constructor(
-    options: {
-      ttl: number;
-      storage: StorageOptionsType;
-    } & Events
-  );
+	constructor(
+		options: {
+			ttl: number;
+			storage: StorageOptionsType;
+		} & Events,
+	);
 
+	define<T extends (...args: any[]) => any>(
+		name: string,
+		opts: {
+			storage?: StorageOptionsType;
+			transformer?: DataTransformer;
+			ttl?: number | ((result: Awaited<ReturnType<T>>) => number);
+			stale?: number;
+			serialize?: (...args: any[]) => any;
+			references?: (
+				args: Parameters<T>,
+				key: string,
+				result: Awaited<ReturnType<T>>,
+			) => References | Promise<References>;
+		} & Events,
+		func?: T,
+	): void;
+	define(name: string, opts: (...args: any[]) => any): void;
 
-  define<T extends (...args: any[]) => any>(
-    name: string,
-    opts: {
-      storage?: StorageOptionsType;
-      transformer?: DataTransformer;
-      ttl?: number | ((result: Awaited<ReturnType<T>>) => number);
-      stale?: number;
-      serialize?: (...args: any[]) => any;
-      references?: (...args: any[]) => References | Promise<References>;
-    } & Events,
-    func?: T
-  ): void;
-  define(
-    name: string,
-    opts: (...args: any[]) => any,
-  ): void;
+	clear(): Promise<void>;
+	clear(name: string, value: any): Promise<void>;
 
-  clear(): Promise<void>;
-  clear(name: string, value: any): Promise<void>;
+	get(name: string, key: string): Promise<any>;
 
-  get(name: string, key: string): Promise<any>;
+	set(name: string, key: string, value: any, ttl: number, references?: References): Promise<void>;
 
-  set(name: string, key: string, value: any, ttl: number, references?: References): Promise<void>;
+	invalidate(name: string, references: References): Promise<void>;
 
-  invalidate(name: string, references: References): Promise<void>;
-
-  invalidateAll(references: References, storage?: StorageOptionsType): Promise<void>;
+	invalidateAll(references: References, storage?: StorageOptionsType): Promise<void>;
 }
 
+declare function createStorage(type: "redis", options: StorageRedisOptions): StorageInterface;
+declare function createStorage(type: "memory", options: StorageMemoryOptions): StorageInterface;
 declare function createStorage(
-  type: "redis",
-  options: StorageRedisOptions
-): StorageInterface;
-declare function createStorage(
-  type: "memory",
-  options: StorageMemoryOptions
-): StorageInterface;
-declare function createStorage(
-  type: StorageOptionsType,
-  options: StorageRedisOptions | StorageMemoryOptions
+	type: StorageOptionsType,
+	options: StorageRedisOptions | StorageMemoryOptions,
 ): StorageInterface;
 
 export { createCache, Cache, createStorage, StorageInterface, StorageMemoryOptions };
--- index.test-d.ts
+++ index.test-d.ts
@@ -6,7 +6,7 @@ import { StorageInterface, StorageMemoryOptions } from "./index.js";
 // Testing internal types
 
 const storageOptions: StorageMemoryOptions = {
-  size: 1000,
+	size: 1000,
 };
 
 const cache = createCache();
@@ -16,49 +16,56 @@ const storage = createStorage("memory", storageOptions);
 expectType<StorageInterface>(storage);
 
 const memoryCache = createCache({
-  storage: {
-    type: "memory",
-    options: storageOptions,
-  },
+	storage: {
+		type: "memory",
+		options: storageOptions,
+	},
 });
 expectType<Cache>(memoryCache);
 
 const cacheWithTtlAndStale = createCache({
-  ttl: 1000,
-  stale: 1000,
+	ttl: 1000,
+	stale: 1000,
 });
 expectType<Cache>(cacheWithTtlAndStale);
 
 // Testing Union Types
 
 const fetchSomething = async (k: any) => {
-  console.log("query", k);
-  return { k };
+	console.log("query", k);
+	return { k };
 };
 
 export type CachedFunctions = {
-  fetchSomething: typeof fetchSomething;
-  fetchSomethingElse: typeof fetchSomething;
-  fetchSomethingElseWithTtlFunction: typeof fetchSomething;
+	fetchSomething: typeof fetchSomething;
+	fetchSomethingElse: typeof fetchSomething;
+	fetchSomethingElseWithTtlFunction: typeof fetchSomething;
 };
 
 const unionMemoryCache = createCache({
-  storage: {
-    type: "memory",
-    options: storageOptions,
-  },
+	storage: {
+		type: "memory",
+		options: storageOptions,
+	},
 }) as Cache & CachedFunctions;
 expectType<Cache & CachedFunctions>(unionMemoryCache);
 
 unionMemoryCache.define("fetchSomething", fetchSomething);
 expectType<typeof fetchSomething>(unionMemoryCache.fetchSomething);
 
-unionMemoryCache.define("fetchSomethingElse", { ttl: 1000, stale: 1000}, fetchSomething);
+unionMemoryCache.define(
+	"fetchSomethingElse",
+	{ ttl: 1000, stale: 1000, references: (args, key, result) => result.k },
+	fetchSomething,
+);
 expectType<typeof fetchSomething>(unionMemoryCache.fetchSomethingElse);
 
-unionMemoryCache.define("fetchSomethingElseWithTtlFunction", { ttl: (result) => result.k ? 1000 : 5, stale: 1000}, fetchSomething);
+unionMemoryCache.define(
+	"fetchSomethingElseWithTtlFunction",
+	{ ttl: (result) => (result.k ? 1000 : 5), stale: 1000 },
+	fetchSomething,
+);
 expectType<typeof fetchSomething>(unionMemoryCache.fetchSomethingElseWithTtlFunction);
-
 
 const result = await unionMemoryCache.fetchSomething("test");
 expectType<{ k: any }>(result);
