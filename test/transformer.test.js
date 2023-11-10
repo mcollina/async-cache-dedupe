cc70131869ce57f8f55b5832f487a47e426c5182
mateonunez <mateonunez95@gmail.com>
2023-11-10 17:53:14 +0100

    chore(transformer): quit redis
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- test/transformer.test.js
+++ test/transformer.test.js
@@ -1,124 +1,123 @@
 'use strict'
 
-const { test, before, after } = require('node:test')
+const { test, describe, after } = require('node:test')
 const assert = require('assert')
 const Redis = require('ioredis')
 
 const createStorage = require('../src/storage')
 const { Cache } = require('../')
 
-let redisClient
-before(async (t) => {
-  redisClient = new Redis()
-})
+const redisClient = new Redis()
 
-after(async (t) => {
-  await redisClient.quit()
-})
+describe('transformer', function () {
+  after(async () => {
+    await redisClient.quit()
+  })
 
-test('should handle a custom transformer to store and get data per cache', async function (t) {
-  const cache = new Cache({
-    ttl: 1000,
-    storage: createStorage(),
-    transformer: {
-      serialize: (value) => {
-        assert.ok('serialize called')
-        return JSON.stringify(value)
-      },
-      deserialize: (value) => {
-        assert.ok('deserialize called')
-        return JSON.parse(value)
+  test('should handle a custom transformer to store and get data per cache', async function (t) {
+    const cache = new Cache({
+      ttl: 1000,
+      storage: createStorage(),
+      transformer: {
+        serialize: (value) => {
+          assert.ok('serialize called')
+          return JSON.stringify(value)
+        },
+        deserialize: (value) => {
+          assert.ok('deserialize called')
+          return JSON.parse(value)
+        }
       }
-    }
+    })
+
+    cache.define('fetchSomething', async (query, cacheKey) => {
+      return { k: query }
+    })
+
+    assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
+    assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
   })
 
-  cache.define('fetchSomething', async (query, cacheKey) => {
-    return { k: query }
-  })
+  test('should handle a custom transformer to store and get data per define', async function (t) {
+    const cache = new Cache({
+      storage: createStorage()
+    })
 
-  assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
-  assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
-})
-
-test('should handle a custom transformer to store and get data per define', async function (t) {
-  const cache = new Cache({
-    storage: createStorage()
-  })
-
-  cache.define('fetchSomething', {
-    ttl: 1000,
-    transformer: {
-      serialize: (value) => {
-        assert.ok('serialize called')
-        return JSON.stringify(value)
-      },
-      deserialize: (value) => {
-        assert.ok('deserialize called')
-        return JSON.parse(value)
+    cache.define('fetchSomething', {
+      ttl: 1000,
+      transformer: {
+        serialize: (value) => {
+          assert.ok('serialize called')
+          return JSON.stringify(value)
+        },
+        deserialize: (value) => {
+          assert.ok('deserialize called')
+          return JSON.parse(value)
+        }
       }
-    }
-  }, async (query, cacheKey) => {
-    return { k: query }
+    }, async (query, cacheKey) => {
+      return { k: query }
+    })
+
+    assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
+    assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
   })
 
-  assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
-  assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
-})
-
-test('should handle a custom transformer and references function to store and get data per cache', async function (t) {
-  const cache = new Cache({
-    ttl: 1000,
-    storage: createStorage(),
-    transformer: {
-      serialize: (value) => {
-        assert.ok('serialize called')
-        return JSON.stringify(value)
-      },
-      deserialize: (value) => {
-        assert.ok('deserialize called')
-        return JSON.parse(value)
+  test('should handle a custom transformer and references function to store and get data per cache', async function (t) {
+    const cache = new Cache({
+      ttl: 1000,
+      storage: createStorage(),
+      transformer: {
+        serialize: (value) => {
+          assert.ok('serialize called')
+          return JSON.stringify(value)
+        },
+        deserialize: (value) => {
+          assert.ok('deserialize called')
+          return JSON.parse(value)
+        }
       }
-    }
-  })
+    })
 
-  cache.define('fetchSomething', {
-    references: (args, key, result) => {
-      assert.ok('references called')
-      return ['some-reference']
-    }
-  }, async (query, cacheKey) => {
-    return { k: query }
-  })
-
-  assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
-  assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
-})
-
-test('should handle a custom transformer and references function to store and get data per define', async function (t) {
-  const cache = new Cache({
-    storage: createStorage()
-  })
-
-  cache.define('fetchSomething', {
-    ttl: 1000,
-    references: (args, key, result) => {
-      assert.ok('references called')
-      return ['some-reference']
-    },
-    transformer: {
-      serialize: (value) => {
-        assert.ok('serialize called')
-        return JSON.stringify(value)
-      },
-      deserialize: (value) => {
-        assert.ok('deserialize called')
-        return JSON.parse(value)
+    cache.define('fetchSomething', {
+      references: (args, key, result) => {
+        assert.ok('references called')
+        return ['some-reference']
       }
-    }
-  }, async (query, cacheKey) => {
-    return { k: query }
+    }, async (query, cacheKey) => {
+      return { k: query }
+    })
+
+    assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
+    assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
   })
 
-  assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
-  assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
+  test('should handle a custom transformer and references function to store and get data per define', async function (t) {
+    const cache = new Cache({
+      storage: createStorage()
+    })
+
+    cache.define('fetchSomething', {
+      ttl: 1000,
+      references: (args, key, result) => {
+        assert.ok('references called')
+        return ['some-reference']
+      },
+      transformer: {
+        serialize: (value) => {
+          assert.ok('serialize called')
+          return JSON.stringify(value)
+        },
+        deserialize: (value) => {
+          assert.ok('deserialize called')
+          return JSON.parse(value)
+        }
+      }
+    }, async (query, cacheKey) => {
+      return { k: query }
+    })
+
+    assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
+    assert.deepStrictEqual(await cache.fetchSomething(42), { k: 42 })
+  })
 })
